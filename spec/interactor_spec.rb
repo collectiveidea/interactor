7b2a8cdfebddc3498380634f261a740b480ecea8
Steve Richert <steve.richert@gmail.com>
2013-07-19 13:05:54 -0400

    Install RSpec

---
+++ spec/interactor_spec.rb
@@ -0,0 +1,5 @@
+require "spec_helper"
+
+describe Interactor do
+  it "is great"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,3 @@
+require "interactor"
+
+Dir[File.expand_path("../support/*.rb", __FILE__)].each { |f| require f }
