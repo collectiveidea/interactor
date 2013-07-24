64a064f711ab9519e07c3300017f7f4d82a978fc
Steve Richert <steve.richert@gmail.com>
2013-07-24 18:22:58 -0400

    Test class-level interactor performance

--- lib/interactor.rb
+++ lib/interactor.rb
@@ -1,2 +1,13 @@
 module Interactor
+  def self.included(base)
+    base.class_eval do
+      extend ClassMethods
+    end
+  end
+
+  module ClassMethods
+    def perform(context = {})
+      new(context).tap(&:perform)
+    end
+  end
 end
--- spec/interactor_spec.rb
+++ spec/interactor_spec.rb
@@ -1,5 +1,23 @@
 require "spec_helper"
 
 describe Interactor do
-  it "is great"
+  let(:interactor) { Class.new { include Interactor } }
+
+  describe ".perform" do
+    let(:instance) { double(:instance) }
+
+    it "performs an instance with the given context" do
+      expect(interactor).to receive(:new).with(foo: "bar") { instance }
+      expect(instance).to receive(:perform).once.with(no_args)
+
+      expect(interactor.perform(foo: "bar")).to eq(instance)
+    end
+
+    it "provides a blank context if none is given" do
+      expect(interactor).to receive(:new).with({}) { instance }
+      expect(instance).to receive(:perform).once.with(no_args)
+
+      expect(interactor.perform).to eq(instance)
+    end
+  end
 end
