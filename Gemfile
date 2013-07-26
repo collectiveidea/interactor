b3f195df2158c9fa46f8824d67a3b0c37c3d76a1
Steve Richert <steve.richert@gmail.com>
2013-07-26 11:12:50 -0400

    Add and install Coveralls

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source "https://rubygems.org"
 gemspec
 
 group :test do
+  gem "coveralls", "~> 0.6.7", require: false
   gem "rspec", "~> 2.14"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require "coveralls"
+Coveralls.wear!
+
 require "interactor"
 
 Dir[File.expand_path("../support/*.rb", __FILE__)].each { |f| require f }
