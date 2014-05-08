c72d502ed546d82ce2bd4a6db5eac6df6c670991
Steve Richert <steve.richert@gmail.com>
2014-05-08 12:51:01 -0400

    Move hook logic out into its own module

---
+++ lib/interactor/hooks.rb
@@ -0,0 +1,59 @@
+module Interactor
+  module Hooks
+    def self.included(base)
+      base.class_eval do
+        extend ClassMethods
+      end
+    end
+
+    module ClassMethods
+      def before(&hook)
+        before_hooks.push(hook)
+      end
+
+      def after(&hook)
+        after_hooks.unshift(hook)
+      end
+
+      def before_hooks
+        @before_hooks ||= []
+      end
+
+      def after_hooks
+        @after_hooks ||= []
+      end
+    end
+
+    private
+
+    def before_hooks
+      self.class.before_hooks
+    end
+
+    def after_hooks
+      self.class.after_hooks
+    end
+
+    def with_hooks
+      call_before_hooks
+      yield
+      call_after_hooks
+    end
+
+    def call_before_hooks
+      call_hooks(before_hooks)
+    end
+
+    def call_after_hooks
+      call_hooks(after_hooks)
+    end
+
+    def call_hooks(hooks)
+      hooks.each { |hook| call_hook(hook) }
+    end
+
+    def call_hook(hook)
+      instance_eval(&hook)
+    end
+  end
+end
