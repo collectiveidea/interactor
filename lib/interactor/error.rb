fc9be3a6e8ca798b45ded189c3e8cbde54d3bed3
Steve Richert <steve.richert@gmail.com>
2014-09-08 11:11:46 -0400

    Add TomDoc documentation to the Interactor::Failure class
    [ci skip]

--- lib/interactor/error.rb
+++ lib/interactor/error.rb
@@ -1,7 +1,28 @@
 module Interactor
+  # Internal: Error raised during Interactor::Context failure. The error stores
+  # a copy of the failed context for debugging purposes.
   class Failure < StandardError
+    # Internal: Gets the Interactor::Context of the Interactor::Failure
+    # instance.
     attr_reader :context
 
+    # Internal: Initialize an Interactor::Failure.
+    #
+    # context - An Interactor::Context to be stored within the
+    #           Interactor::Failure instance. (default: nil)
+    #
+    # Examples
+    #
+    #   Interactor::Failure.new
+    #   # => #<Interactor::Failure: Interactor::Failure>
+    #
+    #   context = Interactor::Context.new(foo: "bar")
+    #   # => #<Interactor::Context foo="bar">
+    #   Interactor::Failure.new(context)
+    #   # => #<Interactor::Failure: #<Interactor::Context foo="bar">>
+    #
+    #   raise Interactor::Failure, context
+    #   # => Interactor::Failure: #<Interactor::Context foo="bar">
     def initialize(context = nil)
       @context = context
       super
