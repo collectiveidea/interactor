bc0fbb605e31a3dea3c3588ec6af15a614ae931d
Kasper Meyer <hi@kaspermeyer.com>
2020-10-19 20:17:02 +0200

    Ensure interactors inherit hooks defined in ancestors

--- spec/interactor/hooks_spec.rb
+++ spec/interactor/hooks_spec.rb
@@ -353,6 +353,110 @@ module Interactor
           ])
         end
       end
+
+      context "with inheritance" do
+        context "with multiple ancestors" do
+          let(:ancestor_top) {
+            build_hooked do
+              around do |interactor|
+                steps << :around_before_ancestor_top
+                interactor.call
+                steps << :around_after_ancestor_top
+              end
+
+              before do
+                steps << :before_ancestor_top
+              end
+
+              after do
+                steps << :after_ancestor_top
+              end
+            end
+          }
+
+          let(:ancestor) {
+            Class.new(ancestor_top) do
+              around do |interactor|
+                steps << :around_before_ancestor
+                interactor.call
+                steps << :around_after_ancestor
+              end
+
+              before do
+                steps << :before_ancestor
+              end
+
+              after do
+                steps << :after_ancestor
+              end
+            end
+          }
+
+          let(:hooked) {
+            Class.new(ancestor) do
+              around do |interactor|
+                steps << :around_before
+                interactor.call
+                steps << :around_after
+              end
+
+              before do
+                steps << :before
+              end
+
+              after do
+                steps << :after
+              end
+            end
+          }
+
+          it "runs hooks defined in ancestors" do
+            expect(hooked.process).to eq([
+              :around_before,
+              :around_before_ancestor,
+              :around_before_ancestor_top,
+              :before,
+              :before_ancestor,
+              :before_ancestor_top,
+              :process,
+              :after_ancestor_top,
+              :after_ancestor,
+              :after,
+              :around_after_ancestor_top,
+              :around_after_ancestor,
+              :around_after
+            ])
+          end
+        end
+
+        describe "with hooks added to ancestors at runtime" do
+          let(:ancestor) {
+            build_hooked do
+              before do
+                steps << :before_at_parse
+              end
+            end
+          }
+
+          let(:hooked) {
+            Class.new(ancestor)
+          }
+
+          before do
+            ancestor.before do
+              steps << :before_at_runtime
+            end
+          end
+
+          it "runs hooks defined in ancestors" do
+            expect(hooked.process).to eq([
+              :before_at_parse,
+              :before_at_runtime,
+              :process
+            ])
+          end
+        end
+      end
     end
   end
 end
